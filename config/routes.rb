1bf901327ecf1d5673e9686615b41ca7a0e82a8c
L Lumbreras <llumbreras@gmail.com>
2013-05-03 11:14:32 -0700

    Install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Inspiredthoughts::Application.routes.draw do
+  devise_for :users
+
   resources :statuses
 
+  # Add for gem devise
+  root :to => "statuses#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
