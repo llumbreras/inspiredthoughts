0a54faa553c5aa9b31085d7364bc2c692e187061
L Lumbreras <llumbreras@gmail.com>
2013-05-08 17:21:12 -0700

    Add custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Inspiredthoughts::Application.routes.draw do
   devise_for :users
 
-  resources :statuses
+  devise_scope :user do
+    get "register", to: "devise/registrations#new", as: :register
+    get "login", to: "devise/sessions#new", as: :login
+    get "logout", to: "devise/sessions#destroy", as: :logout
+  end
 
+  resources :statuses
+  get 'feed', to: "statuses#index", as: :feed
   # Add for gem devise
   root :to => "statuses#index"
 
